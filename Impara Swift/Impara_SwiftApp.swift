0c35fe77b612d95097c662dd068a4a965ecac582
Gabriele Gervasoni <gabrielegervasoni@icloud.com>
2022-02-18 14:51:00 +0100

    Initial Commit

---
+++ Impara Swift/Impara_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Impara_SwiftApp.swift
+//  Impara Swift
+//
+//  Created by Gabriele Gervasoni on 18/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Impara_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
